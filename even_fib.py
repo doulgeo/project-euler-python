501f50804af1dae34bda9b8c0ffda06355a68f18
George Doulgeris <32799331+doulgeo@users.noreply.github.com>
2017-10-14 20:03:32 +0300

    Update even_fib.py
    Added comments.

--- even_fib.py
+++ even_fib.py
@@ -4,20 +4,20 @@ cond = True
 fib = []
 while cond == True:
     n = n0 + n1
-    if n > 4000000:
+    if n > 4000000:   #Check the condition
         cond = False
         break
     else:
         pass
-    if n % 2 ==0 :
-        fib.append(n)
+    if n % 2 ==0 :   #Check if number is even
+        fib.append(n)  #Adds Fibonacci number into list
     else:
         pass
-    n0 = n1
-    n1 = n
+    n0 = n1  #Change of variables to find the next number 
+    n1 = n   #in sequence
 
 total = 0
-for num in fib:
+for num in fib:   #add the numbers in list to find the sum
     total = total + num
 
 print total
