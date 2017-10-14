a1314c9119377ee8edd1b9e0b92658517c3def70
George Doulgeris <32799331+doulgeo@users.noreply.github.com>
2017-10-14 20:28:31 +0300

    Add files via upload
    Finds the largest palindrome that is a product of two 3-digit numbers.

---
+++ palindromes.py
@@ -0,0 +1,12 @@
+c = []
+d = []
+for x in range(111,1000):
+    for y in range(111,1000):
+        prod = str(x * y)
+        for digit in prod:
+            c.append(int(digit))
+        if len(c) != 6:       #Digits must be an even number to have a palindrome. 
+            del c[:]          #Product of two 3-digist factors can't be more than 6 digits.
+        elif (c[0] == c[5]) and (c[1] == c[4]) and (c[2] == c[3]):    #Checks in number is a palindrome
+            d.append(prod)
+print max(d)
