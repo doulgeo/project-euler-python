791ee02a55656a714ffb5347d3ab6a08b904d255
doulgeo <32799331+doulgeo@users.noreply.github.com>
2017-12-10 22:47:15 +0200

    Added description and correction
    Correction was eliminating some variables and needless steps.

--- prime_factors.py
+++ prime_factors.py
@@ -1,19 +1,20 @@
+"""PROBLEM 3:
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
 import time
-
-
 
 def euler(n):
     i = 2        #First prime number relevant in multiplication
     ans = 1
     pfactors = []
-    cond = True
-    while cond == True:
-        if i > n:   #Checks if possible divisor is greater
-            cond = False  #than our divident
-        else:
-            pass
 
-        if n % i == 0:   
+    while True:
+        if i > n:  #Checks if possible divisor is greater
+              break  #than our divident
+        elif n % i == 0:
             pfactors.append(i)
             n = n / i #Makes sure that i is a prime number
             i = 2
@@ -22,8 +23,9 @@ def euler(n):
             i = i + 1
 
 
-    print pfactors
+    return max(pfactors)
 
 start_time = time.time()
+print euler( 600851475143)
 exec_time = time.time() - start_time
 print "time is: ", float(exec_time)
